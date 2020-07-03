acfcd06301ea78e8dc4eb7d0c4607c0b0bbe3c18
vcarrere <vincent@apsulis.com>
2020-07-03 11:06:42 +0200

    add handler

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"asso_api/handlers"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/asso", handlers.GetAssociations())
+	r.GET("/asso", handler.GetAssociations())
 	r.Run()
 }
